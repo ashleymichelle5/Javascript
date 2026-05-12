2b7aed3e1e1ae466fe14c30a22532bb6e5c7009d
Ashley Michelle <ashleymichelle0511@gmail.com>
2026-05-11 22:16:45 -0400

    Initial commit

---
+++ lesson-01/README.md
@@ -0,0 +1,3 @@
+# Exercises 
+![exercise1](exercise1.png)
+![exercise1.2](exercise1.2.png)# Javascript
---
+++ lesson-01/javascript-basics.js
@@ -0,0 +1,13 @@
+alert("Hello, World!");
+alert("This is a test alert.");
+
+10 + 5
+20 - 5
+2 + 2 - 5 
+
+document.body.innerHTML = "Hello, World!";
+
+10 + 8 + 20 
+100 - 20 + 50 + 200
+
+document.body.innerHTML = ' ';
