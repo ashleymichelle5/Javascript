d5cc0a50c469557c92705c0e6a7a674245278107
Ashley Michelle <ashleymichelle0511@gmail.com>
2026-06-02 21:56:47 -0400

    concatenation and interpolation

---
+++ lesson-3/strings.js
@@ -0,0 +1,18 @@
+console.log('my name is ' + 'Ashley');
+
+
+console.log('$' +( 5 + 3));
+console.log('The cost: $' + (5 + 3));
+console.log(`The cost : $${8}`);
+
+console.log((599 + 295) / 100);
+console.log('Total cost: $' + (533 + 295) /100);  //concatenation
+console.log(`Total cost: $${(599 + 295) /100}`); // interpolation
+console.log(`The cost: $${(599+295)/100} \n Thank you, come again`);
+
+//challenge exercises 
+console.log(`Items (4): $${(2095 + 2095 + 799 + 799 ) / 100}`);
+console.log(`Shipping & handling: $${(499 + 499) /100 }`);
+console.log(`Total before tax: $${(5788 + 998)/100}`);
+console.log(`Estimated tax (10%): $${Math.round(67.86 * 0.1 * 100) / 100}`);
+console.log(`Order total: $${(5788 + 998 + 679) /100}`);
